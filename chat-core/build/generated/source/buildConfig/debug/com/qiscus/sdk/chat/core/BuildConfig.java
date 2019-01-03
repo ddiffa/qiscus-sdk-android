e33b07bf7bcf6e5d897df7776e982a61b8f22c1d
ddiffa <ddiffa2@gmail.com>
2019-01-03 11:43:01 +0700

    update dependencies

---
+++ chat-core/build/generated/source/buildConfig/debug/com/qiscus/sdk/chat/core/BuildConfig.java
@@ -0,0 +1,17 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.qiscus.sdk.chat.core;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.qiscus.sdk.chat.core";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.1.3";
+  // Fields from build type: debug
+  public static final String BASE_URL_MQTT_BROKER = "ssl://mqtt.qiscus.com:1885";
+  public static final String BASE_URL_MQTT_LB = "https://mqtt-lb.qiscus.com";
+  public static final String BASE_URL_SERVER = "https://api.qiscus.com/";
+}
